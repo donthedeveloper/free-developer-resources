ac29bbf3a97bb71a1eb4d6dcd658eb2f3e34221a
Don Hansen <donhansen347@gmail.com>
2018-12-22 12:17:32 -0600

    getting ready for github pages

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -3,12 +3,8 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 const config = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
+    apiKey: 'AIzaSyDBLDYHALTWPQbmf8CRBILHWP6n9WdCIO0',
+    projectId: 'free-developer-resources-19062'
 };
 firebase.initializeApp(config);
 firebase.firestore().settings({ timestampsInSnapshots: true });
