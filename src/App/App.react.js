452e9e5207d58df59e4c5c3f33ed1de3ddd67754
Don Hansen <donthedeveloper@users.noreply.github.com>
2018-12-25 20:41:23 -0600

    mvp version of categories

--- src/App/App.react.js
+++ src/App/App.react.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import Admin from './Admin/Admin.react';
+import Categories from './Categories/Categories.react';
 import Navbar from './Navbar/Navbar.react';
 import Profile from './Profile/Profile.react';
 import Resources from './Resources/Resources.react';
 import SignIn from './SignIn/SignIn.react';
 import Signup from './Signup/Signup.react';
+import './App.styles.scss';
 
 const App = () => {
     return (
         <BrowserRouter>
-            <div>
+            <div className='app'>
                 <Navbar />
                 <Switch>
-                    <Route exact path='/resources' component={Resources} />
+                    <Route exact path='/resources' component={Categories} />
                     <Route path='/admin' component={Admin} />
-                    <Route path='/profile' component={Profile} />
-                    <Route path='/signin' component={SignIn} />
-                    <Route path='/signup' component={Signup} />
+                    {/* <Route path='/profile' component={Profile} /> */}
+                    {/* <Route path='/signin' component={SignIn} /> */}
+                    {/* <Route path='/signup' component={Signup} /> */}
                     <Redirect to='/resources' />
                 </Switch>
             </div>
