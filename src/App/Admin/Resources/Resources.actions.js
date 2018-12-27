3f424eec9533099f5fe1dfab16b06b4114dc7a79
Don Hansen <donthedeveloper@users.noreply.github.com>
2018-12-26 22:01:52 -0600

    current progress on resource table

--- src/App/Admin/Resources/Resources.actions.js
+++ src/App/Admin/Resources/Resources.actions.js
@@ -1,23 +1,30 @@
-export const createResource = (resource) => {
-    return (dispatch, getState, { getFirebase, getFirestore }) => {
-        const firestore = getFirestore();
-        firestore.collection('resources').add({
+export const createResource = (resource) =>
+    (dispatch, getState, { getFirebase, getFirestore }) =>
+        getFirestore().collection('resources').add({
             ...resource,
             createdAt: new Date()
         })
             // todo: possibly update success and failures messages?
             // OR figure out how to get 'getFirestore()` in the component (this would keep the flux flow)
+            .then(() => dispatch({ type: 'CREATE_RESOURCE', resource }))
+            .catch((error) => dispatch({ type: 'UPDATE_ERROR', error }));
+
+// export const editResource = (resourceId) =>
+//     (dispatch, getState, { getFirebase, getFirestore }) =>
+//             getFirestore().collection('resources').doc(resourceId).update({
+
+//             })
+
+export const removeResource = (resourceId) =>
+    (dispatch, getState, { getFirebase, getFirestore }) =>
+        getFirestore().collection('resources').doc(resourceId).delete()
             .then(() => {
                 dispatch({
-                    type: 'CREATE_RESOURCE',
-                    resource
-                })
+                    type: 'DELETE_RESOURCE_SUCCESS'
+                });
             })
             .catch((error) => {
                 dispatch({
-                    type: 'UPDATE_ERROR',
-                    error
-                })
+                    type: 'DELETE_RESOURCE_ERROR'
+                });
             });
-    }
-};
--- src/App/Admin/Resources/Resources.react.js
+++ src/App/Admin/Resources/Resources.react.js
@@ -1,30 +1,84 @@
-import React from 'react';
-import { Link, Route, Switch } from 'react-router-dom';
-import CreateResource from './CreateResource/CreateResource.react';
-import ResourceList from './ResourceList/ResourceList.react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import { compose } from 'redux';
+import ResourceForm from './ResourceForm/ResourceForm.react';
+import { removeResource } from './Resources.actions';
 import './Resources.styles.scss';
 
-const Resources = (props) => {
-    return (
-        <div>
-            <div className='resources-navbar'>
-                <ul className='resources-navbar-list'>
-                    <li>
-                        <Link
-                            className='resources-navbar__link'
-                            to={`${props.match.url}/create`}
-                        >
-                            Create Resource
-                        </Link>
-                    </li>
-                </ul>
+class Resources extends Component {
+
+    static defaultProps = {
+        resources: []
+    };
+
+    state = {
+        resourceIsBeingEdited: false
+    };
+
+    saveResourceBeingEdited = () => {
+        
+        // todo: finished?
+        this.toggleEditResource();
+    };
+
+    toggleEditResource() {
+        this.setState({
+            resourceIsBeingEdited: !resourceIsBeingEdited
+        });
+    }
+
+    render() {
+        const resources = this.props.resources;
+
+        return (
+            <div>
+                <table>
+                    <tbody>
+                        <tr>
+                            {/* <th>Difficulty</th> */}
+                            {/* <th>Rating</th> */}
+                            {/* <th>Submitted By</th> */}
+                            <th>Name</th>
+                            <th>Description</th>
+                            <th>Admin</th>
+                        </tr>
+                        {this.props.resources && this.props.resources.map((resource) =>
+
+                            {this.state.resourceIsBeingEdited
+                                ? <ResourceForm resource={resource} onSubmit={this.saveResourceBeingEdited}
+                            }
+
+                            // <tr key={resource.id}>
+                            //     <td>{resource.name}</td>
+                            //     <td>{resource.description}</td>
+                            //     <td>
+                            //         <i className='far fa-edit' onClick={this.toggleEditResource}></i>
+                            //         <i className='far fa-trash-alt' onClick={() => this.props.removeResource(resource.id)}></i>
+                            //     </td>
+                            // </tr>
+                        )}
+                    </tbody>
+                </table>
             </div>
-            <Switch>
-                <Route exact path={`${props.match.url}`} component={ResourceList} />
-                <Route path={`${props.match.url}/create`} component={CreateResource} />
-            </Switch>
-        </div>
-    );
+        );
+    } 
 };
 
-export default Resources;
+const mapStateToProps = state => ({
+    resources: state.firestore.ordered.resources
+});
+
+const mapDispatchToProps = dispatch => ({
+    removeResource: (resourceId) => dispatch(removeResource(resourceId))
+});
+
+export default compose(
+    connect(mapStateToProps, mapDispatchToProps),
+    firestoreConnect([
+        {
+            collection: 'resources',
+            orderBy: ['createdAt', 'desc']
+        }
+    ])
+)(Resources);
