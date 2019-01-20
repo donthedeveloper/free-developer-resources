ff89b6b979f60e76749737d9a4c245f9b1e619a5
Don Hansen <donthedeveloper@users.noreply.github.com>
2019-01-20 12:59:32 -0600

    added disable button logic to resource form and now prevents duplicate resources from being saved

--- src/App/ResourceTable/ResourceForm/ResourceForm.react.js
+++ src/App/ResourceTable/ResourceForm/ResourceForm.react.js
@@ -1,5 +1,9 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import { withRouter } from 'react-router-dom';
+import { compose } from 'redux';
 
 class ResourceForm extends Component {
 
@@ -21,6 +25,7 @@ class ResourceForm extends Component {
             category: resource ? resource.category : 'frontend',
             description: resource ? resource.description : '',
             difficulty: resource ? resource.difficulty : 1,
+            error: '',
             name: resource ? resource.name : '',
             url: resource ? resource.url : ''
         }
@@ -30,6 +35,25 @@ class ResourceForm extends Component {
         this.setState({
             [e.target.name]: e.target.value
         });
+    };
+
+    isResourceNameDuplicate = () => {
+        const resourceBeingEdited = this.props.resource;
+        return this.props.firestoreData.ordered.resources.some((resource) => {
+            if (resourceBeingEdited) {
+                return resourceBeingEdited.id !== resource.id && (resource.name === this.state.name);
+            } else {
+                return resource.name === this.state.name;
+            }
+        });
+    };
+
+    isSubmitDisabled = () => {
+        const { category, description, difficulty, name, url } = this.state;
+        if (category && description && difficulty && name && url) {
+            return false;
+        }
+        return true;
     };
 
     save = (e) => {
@@ -43,21 +67,29 @@ class ResourceForm extends Component {
             resourceToSave.id = resource.id
         }
 
-        this.props.onSubmit(resourceToSave);
-        // todo: clear state mainly for adding a new resource
-        // todo: this assumes it was successful without checking
-        this.setState({
-            category: 'frontend',
-            description: '',
-            difficulty: 1,
-            name: '',
-            url: ''
-        })
+        if (this.isResourceNameDuplicate()) {
+            this.setState({
+                error: 'Resource name should be unique.'
+            });
+        } else {
+            this.props.onSubmit(resourceToSave);
+            // todo: clear state mainly for adding a new resource
+            // todo: this assumes it was successful without checking
+            this.setState({
+                category: 'frontend',
+                description: '',
+                difficulty: 1,
+                error: '',
+                name: '',
+                url: ''
+            });
+        }
     };
 
     render() {
         return (
             <form onSubmit={this.save}>
+                <p>{this.state.error}</p>
                 <label htmlFor='name'>Name</label>
                 <input
                     id='name'
@@ -105,10 +137,20 @@ class ResourceForm extends Component {
                     type='text'
                     value={this.state.description}
                 />
-                <button type='submit'>Save</button>
+                <button disabled={this.isSubmitDisabled()} type='submit'>Save</button>
             </form>
         );
     }
 }
 
-export default ResourceForm;
+const mapStateToProps = state => ({
+    firestoreData: state.firestore
+});
+
+export default compose(
+    connect(mapStateToProps),
+    firestoreConnect([
+        { collection: 'resources' }
+    ]),
+    withRouter
+)(ResourceForm);
